67f0f92bd2a73a97988b25c221bd5bbdac7850ed
sammykim <kimsamm@oregonstate.edu>
2024-02-04 11:36:37 -0800

    TS and Harpoon added.

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 require("syk17.lazy")
 require("syk17.config.remap")
 require("syk17.config.options")
+require("syk17.plugins.nvim-treesitter")
---
+++ lua/syk17/plugins/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = { enable = true },
+        ensure_installed = {
+          "json",
+          "javascript",
+          "html",
+          "css",
+          "markdown",
+          "markdown_inline",
+          "lua",
+          "vim",
+          "query",
+          "python",
+        },
+      })
+    end,
+  },
+}
