95ee1bda9525a384125516e532014865387eccbb
sammykim <kimsamm@oregonstate.edu>
2024-02-04 18:06:42 -0800

    Fixed colorscheme priority loading
    Colorscheme no longer changes when Lazy manager is run

--- lua/syk17/plugins/colorscheme.lua
+++ lua/syk17/plugins/colorscheme.lua
@@ -1,24 +1,25 @@
 return {
-  {
-  'SYK17/Mies.vim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-     vim.cmd([[colorscheme mies]])
- end,
-},
+    {
+        'SYK17/Mies.vim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme mies]])
+        end,
+    },
 
-{'SYK17/rams.vim',
-    config = function()
-        vim.cmd([[colorscheme rams]])
- end,
-},
+    {   'SYK17/rams.vim',
+        lazy = true,
+        config = function()
+            vim.cmd([[colorscheme rams]])
+        end,
+    },
 
-{
-  'SYK17/candle-grey',
-    config = function()
-      vim.cmd([[colorscheme candle-grey-transparent]])
- end,
-},
-
+    {
+        'SYK17/candle-grey',
+        lazy = true,
+        config = function()
+            vim.cmd([[colorscheme candle-grey-transparent]])
+        end,
+    },
 }
