9ee852f7191cb3295856e883922e5d7d6fde922a
sammykim <kimsamm@oregonstate.edu>
2024-09-14 21:07:58 -0700

    [minor] Config

--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -9,7 +9,7 @@ return {
         require("nvim-tree").setup {
             sort_by = "case_sensitive",
             view = {
-                width = 30,
+                width = 33,
             },
             renderer = {
                 group_empty = true,
