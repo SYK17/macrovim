328c558498fe2fd78deff8d28addbf13c8ce9c98
sammykim <kimsamm@oregonstate.edu>
2024-09-15 08:19:58 -0700

    [minor] Config

--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -1,10 +1,11 @@
 return {
     "nvim-tree/nvim-tree.lua",
     version = "*",
-    lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
+    lazy = true,
+    cmd = { "NvimTreeToggle" },
+    -- dependencies = {
+    --     "nvim-tree/nvim-web-devicons",
+    -- },
     config = function()
         require("nvim-tree").setup {
             sort_by = "case_sensitive",
--- lua/core/plugins/telescope-undo.lua
+++ lua/core/plugins/telescope-undo.lua
@@ -1,11 +1,11 @@
 return {
     "debugloop/telescope-undo.nvim",
-    dependencies = { -- note how they're inverted to above example
-        {
-            "nvim-telescope/telescope.nvim",
-            dependencies = { "nvim-lua/plenary.nvim" },
-        },
-    },
+    -- dependencies = { -- note how they're inverted to above example
+    --     {
+    --         "nvim-telescope/telescope.nvim",
+    --         dependencies = { "nvim-lua/plenary.nvim" },
+    --     },
+    -- },
     keys = {
         { -- lazy style key map
             "<leader>u",
